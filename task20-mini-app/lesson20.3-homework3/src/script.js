5f8fa7a1a0ee5e78434936c1023dd2424ee18eca
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-04-16 16:56:26 +0600

    Delete task20-mini-app/lesson20.3-homework3 directory

--- task20-mini-app/lesson20.3-homework3/src/script.js
+++
@@ -1,9 +0,0 @@
-const burgerMenu = document.querySelector(".burger-menu");
-const navMenu = document.querySelector(".nav2-container");
-
-burgerMenu.addEventListener("click", openBurgerMenu);
-
-function openBurgerMenu() {
-  burgerMenu.classList.toggle("close-menu");
-  navMenu.classList.toggle("nav2-container-active");
-}
